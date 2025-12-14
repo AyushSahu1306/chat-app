959d9938006fdd001b5f86137e1fcf1b9e61e88e
Ayush Sahu <sahuayush0613@gmail.com>
2025-12-14 22:37:25 +0530

    feat: Implement authentication controllers and middleware with Cloudinary integration

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { login, logout, signup, updateProfile } from "../controllers/auth.controllers.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,6 +9,6 @@ router.post("/signup",signup);
 router.post("/login",login);
 router.post("/logout",logout);
 
-router.put("/update-profile",updateProfile);
+router.put("/update-profile",protectRoute,updateProfile);
 
 export default router;
