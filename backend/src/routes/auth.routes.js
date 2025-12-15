7b11c9e1c2148b77b251c57a727fd87e024e9a47
Ayush Sahu <sahuayush0613@gmail.com>
2025-12-15 12:56:58 +0530

    feat: Add messaging functionality with message routes and controllers

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, signup, updateProfile } from "../controllers/auth.controllers.js";
+import { login, logout, signup, updateProfile,checkAuth } from "../controllers/auth.controllers.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -10,5 +10,6 @@ router.post("/login",login);
 router.post("/logout",logout);
 
 router.put("/update-profile",protectRoute,updateProfile);
+router.get("/check",protectRoute,checkAuth);
 
 export default router;
